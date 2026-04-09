9d30f42724704cf8056232832ccfc0783564b171
oppestana <pedro.t.pestana@gmail.com>
2026-04-09 16:12:41 -0300

    Inimigos

---
+++ D20.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sessao3
+{
+    public static class D20
+    {
+        public static void Executar()
+        {
+
+            string zombieWalkes = "Corredor";
+            string zombieSmoker = "Linguarudo";
+            string zombieRotters = "Infectado";
+            string skeletonCaptain ="Capitão Esqueleto";
+            string skeletonWarrior ="Guerreiro Esqueleto";
+            string skeletonGunner = "Esqueleto atirador";
+            string mageWater = "Mago de Água";
+            string mageFire = "Mago de Fogo";
+            string mageLightning = "Mago de Raio";
+
+        }
+    }
+}
